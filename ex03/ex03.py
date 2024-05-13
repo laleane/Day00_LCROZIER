caba89303096c897cc4780487739ae5dfd939779
leane crozier <148325124+laleane@users.noreply.github.com>
2024-05-13 12:43:44 +0200

    Ajout ex03
    ex03 complété

---
+++ ex03/ex03.py
@@ -0,0 +1,7 @@
+def alphabet_reverse():
+    alphabet = "abcdefghijklmnopqrstuvwxyz"
+    for letter in reversed(alphabet):
+        print(letter)
+
+# Appel de la fonction pour écrire chaque lettre de l'alphabet en partant de "z"
+alphabet_reverse()
