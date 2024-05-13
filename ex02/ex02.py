93da96c05d990b58f30a2b4c82ceb2e3626e9bae
leane crozier <148325124+laleane@users.noreply.github.com>
2024-05-13 12:41:28 +0200

    Ajout ex02
    ex02 complété

---
+++ ex02/ex02.py
@@ -0,0 +1,7 @@
+def alphabet_letters():
+    alphabet = "abcdefghijklmnopqrstuvwxyz"
+    for letter in alphabet:
+        print(letter)
+
+# Appel de la fonction pour écrire chaque lettre de l'alphabet
+alphabet_letters()
